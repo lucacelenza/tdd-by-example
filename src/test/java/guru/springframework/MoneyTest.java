ba2e14952cc3de30e3619085bd5e03da57701a31
Luca Celenza <celenzaluca@gmail.com>
2021-02-19 10:47:37 +0100

    Added code for chapter 1

---
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -0,0 +1,22 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MoneyTest {
+
+    @Test
+    void testFiveTimesTwo() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.getAmount());
+    }
+
+    @Test
+    void testFiveTimesThree() {
+        Dollar five = new Dollar(5);
+        five.times(3);
+        assertEquals(15, five.getAmount());
+    }
+}
