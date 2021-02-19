e9d4af3e73564dad7f0d60628878ece375c4415f
Luca Celenza <celenzaluca@gmail.com>
2021-02-19 14:22:46 +0100

    Added code for chapter 6

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -1,32 +1,11 @@
 package guru.springframework;
 
-import java.util.Objects;
-
-public class Dollar {
-    private final int amount;
-
+public class Dollar extends Money {
     public Dollar(int amount) {
-        this.amount = amount;
+        super(amount);
     }
 
     public Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Dollar)) return false;
-        Dollar dollar = (Dollar) o;
-        return amount == dollar.amount;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(amount);
     }
 }
--- src/main/java/guru/springframework/Franc.java
+++ src/main/java/guru/springframework/Franc.java
@@ -1,32 +1,11 @@
 package guru.springframework;
 
-import java.util.Objects;
-
-public class Franc {
-    private final int amount;
-
+public class Franc extends Money {
     public Franc(int amount) {
-        this.amount = amount;
+        super(amount);
     }
 
     public Franc times(int multiplier) {
         return new Franc(amount * multiplier);
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Franc)) return false;
-        Franc franc = (Franc) o;
-        return amount == franc.amount;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(amount);
     }
 }
